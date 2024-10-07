c926ad1461671a21876c817c20024f3b458b36f2
Dlandaburu <mbutusi1@educacion.navarra.es>
2024-10-07 19:33:16 +0200

    Tarea medio hecha de Algoritmos 1.1
    ...

---
+++ 1.1 Algoritmos Adicionales/algoritmos.java
@@ -0,0 +1,82 @@
+//hello World//
+
+System.out.println("Hello World");
+
+//edad//
+
+public class Main {
+
+    class edad{
+    edad (= 20);
+    system.out.print(edad);
+    }
+}
+
+//variables a declarar//
+
+public class Main {
+class Variable{
+
+int x=5;
+int y=10;
+
+system.out.print(x + y);
+}
+}
+
+//Random number//
+
+public class Main {
+    class Random{
+        x = 20{
+            if x > 10;
+        }
+        system.out.print(x) 
+    }
+}
+
+//cuadrado//
+
+public class Main {
+    class Cuadrado {
+        int base = 4;
+        int altura =5;
+    }
+    system.out.print(base * altura);
+}
+
+//number user//
+
+public class Main {
+    class Pares{
+    function esPar(numero) {
+  return numero % 2 === 0;
+}
+
+    function esImpar(numero) {
+  return numero % 2 !== 0;
+}
+
+    if function esPar;
+    }
+    system.out.print(esPar);
+}
+
+//Ciclo 1-5//
+
+public class Main {
+    public static void main(String[] args) {
+        for int(i = 1; i <= 5){
+            system.out.println(i)
+        }
+    }
+}
+
+//Los 10 primeros números pares//
+public class Main {
+    public static void main(String[] args) {
+        for int(i = 0; i <= 10; i = i + 2;  ){
+            system.out.println(i)
+        }
+    }
+}
